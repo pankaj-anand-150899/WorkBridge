f2aac2bd748546e0a0a5dd74d6eb3e1d754d5f97
pankaj_anand <pankajanand150899@gmail.com>
2025-06-24 06:35:24 +0530

    project added to git repo

---
+++ workbridge/src/main/java/com/anand/workbridge/WorkbridgeApplication.java
@@ -0,0 +1,13 @@
+package com.anand.workbridge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkbridgeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkbridgeApplication.class, args);
+	}
+
+}
---
+++ workbridge/src/test/java/com/anand/workbridge/WorkbridgeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anand.workbridge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkbridgeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
